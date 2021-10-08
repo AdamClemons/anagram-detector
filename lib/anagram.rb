9ea9df4fda7e6e9e8a849dfcdf93a4f5fe5f00b6
Adam Clemons <74882592+thisClemons@users.noreply.github.com>
2021-10-07 20:17:25 -0400

    commit tree 9a660349611ce984f8c97e0932c93159b1aae821

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word        
+    end
+
+    def match(string)
+        matches = []
+
+        string.each do |piece|
+            # binding.pry
+            matches << piece if piece.split("").sort == word.split("").sort
+        end
+
+        matches
+    end
+    
+end
